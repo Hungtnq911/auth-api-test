a89645919fd89d6093bf5ee8664e6cf4ede19d01
@HungFado00 <hungtnq@miczone.asia>
2022-08-22 12:38:35 +0700

    -GET APIs send workspacejson when we login

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
+const { description } = require('@hapi/joi/lib/base');
 const User = require('../model/User');
-const verify = require('./verifyToken');
-
+const {requireAuth} = require('./verifyToken');
+const objectID =require('mongoose').Types.ObjectId;
 //use when user have access
-router.get('/',verify ,(req,res) => {
-    res.send(req.user);
+router.get('/',requireAuth ,async (req,res) => {
+    // get id
+    const userID = req.user._id;
+    // search for workspace and send back to client
+    const workspaceID = await User.findOne(
+        {_id :objectID(userID)},
+        {_id : 0,workspaceID: 1}
+    );
+
+    res.send(workspaceID);
     
 });
-
-module.exports = router;
