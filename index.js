5ba1c49709e2f1a25451c28b685d5702c48134ea
@HungFado00 <hungtnq@miczone.asia>
2022-08-22 12:36:02 +0700

    - give execution when try access to database

--- index.js
+++ index.js
@@ -2,24 +2,38 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 //import routes
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
 
 dotenv.config();
 
+
+
 //connect to DB
-mongoose.connect(
-    process.env.DB_CONNECT,
-    () => console.log('connected to DB')
-);
+const connectDB = async () =>{
+    try {
+        await mongoose.connect(
+            process.env.DB_CONNECT,{
+                useNewUrlParser: true,
+            });
+            console.log('MongoDB connected!!');
+    } catch (err) {
+        console.log('Failed to connect to MongoDB', err);
+    }
+};
+
+connectDB();
+
 
 //middleware
 app.use(express.json());
-
+app.use(cookieParser());
 
 
 //Route middlewares
+
 app.use('/api/user',authRoute);
 app.use('/api/posts',postRoute);
 
